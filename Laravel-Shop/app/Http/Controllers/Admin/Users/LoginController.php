add38303b6749595104e1fe984dfedf034bf339d
huykt1111 <huylmht10@gmail.com>
2022-11-03 21:36:03 +0700

    login

--- Laravel-Shop/app/Http/Controllers/Admin/Users/LoginController.php
+++ Laravel-Shop/app/Http/Controllers/Admin/Users/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Auth;
+use Session;
 class LoginController extends Controller
 {
     public function index()
@@ -13,4 +14,22 @@ class LoginController extends Controller
             'title' => 'Đăng nhập hệ thống'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ],$request->input('remember'))){
+            return redirect()->route('admin');
+        }
+        Session::flash('error','Email or Password is incorrect');
+        return redirect()->back();
+    }
+
 }
