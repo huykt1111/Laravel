b70ce1eaaacc94a5034089db2096c6ab975bb8a5
huykt1111 <huylmht10@gmail.com>
2022-11-08 18:42:43 +0700

    Insert - Update - Delete

--- Laravel-Shop/routes/web.php
+++ Laravel-Shop/routes/web.php
@@ -5,6 +5,7 @@ use \App\Http\Controllers\Admin\Users\LoginController;
 use \App\Http\Controllers\Admin\MainController;
 use \App\Http\Controllers\Admin\MenuController;
 use \App\Http\Controllers\Admin\ProductController;
+use \App\Http\Controllers\Admin\SliderController;
 
 Route::get('admin/users/login',[LoginController::class, 'index'])->name('login');
 Route::post('admin/users/login/store',[LoginController::class, 'store']);
@@ -33,6 +34,16 @@ Route::middleware(['auth'])->group(function(){
             Route::get('edit/{product}',[ProductController::class, 'show']);
             Route::post('edit/{product}',[ProductController::class, 'update']);
             Route::DELETE('destroy',[ProductController::class, 'destroy']);
+        });
+
+        #Slider
+        Route::prefix('sliders')->group(function () {
+            Route::get('add', [SliderController::class, 'create']);
+            Route::post('add', [SliderController::class, 'store']);
+            Route::get('list', [SliderController::class, 'index']);
+            Route::get('edit/{slider}', [SliderController::class, 'show']);
+            Route::post('edit/{slider}', [SliderController::class, 'update']);
+            Route::DELETE('destroy',[SliderController::class, 'destroy']);
         });
 
         #Upload
