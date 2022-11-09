1b2da0f7326d14631773b70007b0fe5740a7d349
huykt1111 <huylmht10@gmail.com>
2022-11-09 09:51:31 +0700

    Load Menu and Load Slider

--- Laravel-Shop/app/Http/Controllers/MainController.php
+++ Laravel-Shop/app/Http/Controllers/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Services\Slider\SliderService;
 
 class MainController extends Controller
 {
+    protected $slider;
+
+    public function __construct(SliderService $slider)
+    {
+        $this->slider = $slider;
+    }
+
     public function index()
     {
-        return view('main', [
-            'title' => 'Shop nước hoa ABC'
+        return view('main',[
+           'title' => 'Shop áo quần Quang Huy',
+           'sliders' => $this->slider->show()
+           
         ]);
     }
 }
