fcb6a74d52802b01d0c9a4e51bc02cc68d275981
huykt1111 <huylmht10@gmail.com>
2022-12-29 14:28:19 +0700

    fix cart

--- Laravel-Shop/resources/views/header.blade.php
+++ Laravel-Shop/resources/views/header.blade.php
@@ -59,7 +59,8 @@
                         <i class="zmdi zmdi-search"></i>
                     </div>
 
-                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="0">
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" 
+                        data-notify="{{ count(\Illuminate\Support\Facades\Session::get('carts')) }}">
                         <i class="zmdi zmdi-shopping-cart"></i>
                     </div>
 
@@ -84,7 +85,8 @@
                 <i class="zmdi zmdi-search"></i>
             </div>
 
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="0">
+            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" 
+                data-notify="{{ count(\Illuminate\Support\Facades\Session::get('carts')) }}">
                 <i class="zmdi zmdi-shopping-cart"></i>
             </div>
 
