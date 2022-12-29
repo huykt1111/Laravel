e65cef9c01313fdee46e391f9f5de053d05c12a1
huykt1111 <huylmht10@gmail.com>
2022-12-29 10:19:58 +0700

    Fix cart

--- Laravel-Shop/resources/views/products/list.blade.php
+++ Laravel-Shop/resources/views/products/list.blade.php
@@ -1,24 +1,35 @@
 <div class="row isotope-grid">
     @foreach($products as $key => $product)
-        <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
-            <!-- Block2 -->
-            <div class="block2">
-                <div class="block2-pic hov-img0">
-                    <img src="{{ $product->thumb }}" alt="{{ $product->name }}">
-                </div>
-
-                <div class="block2-txt flex-w flex-t p-t-14">
-                    <div class="block2-txt-child1 flex-col-l ">
-                        <a href="/san-pham/{{ $product->id }}-{{ Str::slug($product->name, '-') }}.html"
-                           class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                            {{ $product->name }}
+        
+        <div class="col col-lg-3 col-md-6 col-sm-6 col-6 isotope-item women">
+            <div class="col-product__item sale-home product-item__sale-off--none">
+                <form action="/add-cart" method="post">
+                    
+                    @csrf
+                    <div>
+                        <a href="">
+                            <i data-heart="130" class="icon-heart-element product-item-icon far fa-heart"></i>
                         </a>
-
-                        <span class="stext-105 cl3">
-							{!!  \App\Helpers\Helper::price($product->price, $product->price_sale)  !!}
-                        </span>
                     </div>
-                </div>
+                    <div class="product-img">
+                        <a href="/san-pham/{{ $product->id }}-{{ Str::slug($product->name, '-') }}.html">
+                            <span class="img--hover"></span> 
+                            <img src="{{ $product->thumb }}" alt="{{ $product->name }}">
+                        </a>
+                        <p class="text-sale">Sale</p>
+                    </div>
+                    <div class="product-fruits__infos">
+                        <h2 class="tilte-name-product">{{ $product->name }}</h2>
+                        <div>
+                            <input type="hidden" class="mtext-104 cl3 txt-center num-product" type="number"
+                                name="num_product" value="1">
+                            <span class="price-new">{!!  \App\Helpers\Helper::price($product->price, $product->price_sale)  !!}</span>
+                            <button type="submit" class="button-add-product button-add-product btn-add-cart button-add-product--view">Cho vào giỏ</button>
+                            <span class="price-old">0đ</span>   
+                            <input type="hidden" name="product_id" value="{{ $product->id }}">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     @endforeach
