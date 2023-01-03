0e356856e05bd7987765d093a89ebaec6c73d21d
Trần Tiến <84917848+trantien2001@users.noreply.github.com>
2023-01-03 16:51:28 +0700

    init commit

---
+++ Laravel-Shop/app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Menu\CreateFormRequest;
+use Illuminate\Http\Request;
+use App\Http\Services\Menu\MenuService;
+use Illuminate\Http\JsonResponse;
+use App\Models\Menu;
+
+class MenuController extends Controller
+{
+    protected $menuService;
+
+    public function __construct(MenuService $menuService)
+    {
+        $this->menuService = $menuService;
+    }
+
+    public function index()
+    {
+        return view('admin.menu.list',[
+            'title' => 'Danh sách danh mục mới nhất',
+            'menus' => $this->menuService->getAll()
+        ]);
+    }
+}
