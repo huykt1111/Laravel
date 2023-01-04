1563c8cdb375c99a3d11730612efc40566f97a43
Nam <ntannam.tn@gmail.com>
2023-01-04 18:54:08 +0700

    footer

---
+++ Laravel-Shop/resources/views/footer.blade.php
@@ -0,0 +1,336 @@
+<!-- Footer -->
+<footer id="footer">
+    <div class="container">
+        <div class="footer-top row">
+            <div class="col col-6 col-lg-3">
+                <a href="" class="footer-top__logo">
+                    <img src="/template/images/logo.webp" alt="">
+                </a>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="box-item-info">
+                    <img src="/template/images/ser_1.webp" alt="">
+                    <div class="content-info">
+                        <h3>Tiết kiệm hơn</h3>
+                        <span>Tiết kiệm hơn nhờ giảm giá thường xuyên</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="box-item-info">
+                    <img src="/template/images/ser_2.png" alt="">
+                    <div class="content-info">
+                        <h3>Khách hàng VIP</h3>
+                        <span>Đăng kí thành viên để nhận thẻ ưu đãi riêng</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-6 col-lg-3">
+                <div class="box-item-info">
+                    <img src="/template/images/ser_3.png" alt="">
+                    <div class="content-info">
+                        <h3>Chất lượng cao</h3>
+                        <span>Sản phẩm chất lượng là ưu tiên hàng đầu</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-center row">
+            <div class="col col-6 col-lg-3">
+                <h2 class="title-menu">Liên hệ với chúng tôi</h2>
+                <ul class="list-address">
+                    <li>
+                        <i class="fas fa-map-marker-alt"></i>
+                        <p>Thanh Bình, Hải Châu, Đà Nẵng, Việt Nam</p>
+                    </li>
+                    <li>
+                        <i class="fas fa-phone"></i>
+                        <a href="">0947895039</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-envelope-square"></i>
+                        <a href="">nqhuy@gmail.com</a>
+                    </li>
+                    <li>
+                        <h2 class="title-menu">Đăng ký nhận tin khuyến mãi</h2>
+                    </li>
+                </ul>
+                <form action="">
+                    <input type="text" class="seed-mail" placeholder="Nhập email của bạn...">
+                </form>
+            </div>
+            <div class="col col-6 col-lg-2">
+                <h2 class="title-menu">Dịch vụ 24/7</h2>
+                <ul class="list-service">
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Sản phẩm</a></li>
+                    <li><a href="">Bánh kẹo</a></li>
+                    <li><a href="">Đồ khô</a></li>
+                    <li><a href="">Liên hệ</a></li>
+                    <li><a href="">Hệ thống bán hàng</a></li>
+                    <li><a href="">Tin tức</a></li>
+                </ul>
+            </div>
+            <div class="col col-4 col-lg-2">
+                <h2 class="title-menu">Dịch vụ 24/7</h2>
+                <ul class="list-service">
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Sản phẩm</a></li>
+                    <li><a href="">Bánh kẹo</a></li>
+                    <li><a href="">Đồ khô</a></li>
+                    <li><a href="">Liên hệ</a></li>
+                    <li><a href="">Hệ thống bán hàng</a></li>
+                    <li><a href="">Tin tức</a></li>
+                </ul>
+            </div>
+            <div class="col col-4 col-lg-2">
+                <h2 class="title-menu">Dịch vụ 24/7</h2>
+                <ul class="list-service">
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Sản phẩm</a></li>
+                    <li><a href="">Bánh kẹo</a></li>
+                    <li><a href="">Đồ khô</a></li>
+                    <li><a href="">Liên hệ</a></li>
+                    <li><a href="">Hệ thống bán hàng</a></li>
+                    <li><a href="">Tin tức</a></li>
+                </ul>
+            </div>
+            <div class="col col-4 col-lg-3">
+                <h2 class="title-menu">Dịch vụ 24/7</h2>
+                <ul class="list-service">
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Sản phẩm</a></li>
+                    <li><a href="">Bánh kẹo</a></li>
+                    <li><a href="">Đồ khô</a></li>
+                    <li><a href="">Liên hệ</a></li>
+                    <li><a href="">Hệ thống bán hàng</a></li>
+                    <li><a href="">Tin tức</a></li>
+                </ul>
+                <img src="/template/images/payment_method.webp" alt="">
+            </div>
+
+        </div>
+        <div class="row footer-bottom">
+            <div class="col col-12 col-lg-7">
+                Bản quyền thuộc về <a href="" style="color:var(--cl-green);">Nhóm 6MPRO</a> Cung cấp bởi SAPA
+            </div>
+            <div class="col col-12 col-lg-5 footer-nav">
+                <ul>
+                    <li><a href="">Trang chủ</a></li>
+                    <li><a href="">Sản phẩm</a></li>
+                    <li><a href="">Bánh kẹo</a></li>
+                    <li><a href="">Đồ khô, gạo</a></li>
+                    <li><a href="">Liên hệ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="box-button-top" title="Go to top">
+        <span class="back-top">
+            <i class="fas fa-arrow-up"></i>
+        </span>
+    </div>
+</footer>
+
+<div class="bottom-nav-bar-mobile">
+    <div class="inner-group container">
+        <div class="itembar">
+            <div class="bar-menu">
+                <div class="icon">
+                    <i class="fas fa-home"></i>
+                </div>
+                <a href="?act=home">Trang chủ</a>
+            </div>
+        </div>
+        <div class="itembar">
+            <div class="bar-menu category-action">
+                <div class="icon">
+                    <i class="fas fa-bars"></i>
+                </div>
+                <span>Danh mục</span>
+            </div>
+        </div>
+        <div class="itembar center">
+            <div class="bar-menu cartbar-mobile" data-count-pr="0">
+                <?php
+                $soluong = 0;
+                $thanhtien = 0;
+                $countProduct = 0;
+                if (isset($_SESSION['product'])) {
+                    foreach ($_SESSION['product'] as $value) {
+                        $countProduct++;
+                        $soluong += 1;
+                        $thanhtien += $value['ThanhTien'];
+                    }
+                }
+                ?>
+                <div class="icon">
+                    <i class="fas fa-cart-plus"></i>
+                    <span class="cart-icon__count"><?php echo $countProduct; ?></span>
+                </div>
+                <a href="?act=cart">Giỏ hàng</a>
+            </div>
+        </div>
+        <div class="itembar">
+            <div class="bar-menu">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <a href="/account/login">Tài khoản</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="zalo-chat-widget" data-oaid="3333736547818229512" data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+    data-autopopup="1" data-width="" data-height=""></div>
+<div id="chat-mess">
+    <a href="https://www.facebook.com/messages/t/104735905436003" target="_blank">
+        <img src="https://img.icons8.com/color/48/000000/facebook-messenger--v5.png" />
+    </a>
+</div>
+<script src="https://sp.zalo.me/plugins/sdk.js"></script>
+
+<!--===============================================================================================-->
+<script src="/template/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<!--===============================================================================================-->
+<script src="/template/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/bootstrap/js/popper.js"></script>
+<script src="/template/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/select2/select2.min.js"></script>
+<script>
+    $(".js-select2").each(function() {
+        $(this).select2({
+            minimumResultsForSearch: 20,
+            dropdownParent: $(this).next('.dropDownSelect2')
+        });
+    })
+</script>
+<!--===============================================================================================-->
+<script src="/template/vendor/daterangepicker/moment.min.js"></script>
+<script src="/template/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/slick/slick.min.js"></script>
+<script src="/template/js/slick-custom.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/parallax100/parallax100.js"></script>
+<script>
+    $('.parallax100').parallax100();
+</script>
+<!--===============================================================================================-->
+<script src="/template/vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
+<script>
+    $('.gallery-lb').each(function() { // the containers for all your galleries
+        $(this).magnificPopup({
+            delegate: 'a', // the selector for gallery item
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+            mainClass: 'mfp-fade'
+        });
+    });
+</script>
+<!--===============================================================================================-->
+<script src="/template/vendor/isotope/isotope.pkgd.min.js"></script>
+<!--===============================================================================================-->
+<script src="/template/vendor/sweetalert/sweetalert.min.js"></script>
+<script>
+    $('.js-addwish-b2').on('click', function(e) {
+        e.preventDefault();
+    });
+
+    $('.js-addwish-b2').each(function() {
+        var nameProduct = $(this).parent().parent().find('.js-name-b2').html();
+        $(this).on('click', function() {
+            swal(nameProduct, "is added to wishlist !", "success");
+
+            $(this).addClass('js-addedwish-b2');
+            $(this).off('click');
+        });
+    });
+
+    $('.js-addwish-detail').each(function() {
+        var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').html();
+
+        $(this).on('click', function() {
+            swal(nameProduct, "is added to wishlist !", "success");
+
+            $(this).addClass('js-addedwish-detail');
+            $(this).off('click');
+        });
+    });
+
+    /*---------------------------------------------*/
+
+    $('.js-addcart-detail').each(function() {
+        var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').html();
+        $(this).on('click', function() {
+            swal(nameProduct, "is added to cart !", "success");
+        });
+    });
+</script>
+<!--===============================================================================================-->
+<script src="/template/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script>
+    $('.js-pscroll').each(function() {
+        $(this).css('position', 'relative');
+        $(this).css('overflow', 'hidden');
+        var ps = new PerfectScrollbar(this, {
+            wheelSpeed: 1,
+            scrollingThreshold: 1000,
+            wheelPropagation: false,
+        });
+
+        $(window).on('resize', function() {
+            ps.update();
+        })
+    });
+</script>
+<!--===============================================================================================-->
+<script src="/template/js/main.js"></script>
+<script src="/template/js/public.js"></script>
+<script src="/template/js/countdown.js"></script>
+<script src="/template/js/owl.carousel.min.js"></script>
+<script src="/template/js/main-wf.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+<script>
+    var $sliderBanner = $('.slider-list')
+    $sliderBanner.owlCarousel({
+        margin: 10,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            1000: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            500: {
+                items: 1
+            }
+        }
+    });
+</script>
+<script>
+    function start() {
+        const timeLeft = JSON.parse(localStorage.getItem('timeLeft'));
+        if (timeLeft == null) {
+            localStorage.setItem('timeLeft', JSON.stringify({
+                d: 7,
+                h: 0,
+                m: 0,
+                s: 0,
+            }))
+        } else {
+            return
+        }
+    }
+    start()
+</script>
